e4387f39b77f770c9db91e2689c3e3c7fea90798
Tera Schaller <tschaller722@gmail.com>
2017-07-22 09:09:13 -0500

    Python Drawing
    Use Python 3.x and turtle module to draw spirals and fractals.

---
+++ myTurtle003.py
@@ -0,0 +1,29 @@
+#Tera Schaller 7/20/2017
+#Udacuty fundamentals of Python tutorial
+#Use turtle module to draw fractals
+#Python 3.6.1
+
+import turtle
+
+def draw_square(myTurtle, size):
+    for i in range(4):
+        myTurtle.forward(size)
+        myTurtle.right(90)
+
+def draw_art(turtleShape, bg_color, art_color, size):
+    window = turtle.Screen()
+    window.bgcolor(bg_color)
+
+    mike = turtle.Turtle()
+    mike.shape(turtleShape)
+    mike.color(art_color)
+    mike.speed(25)
+
+    for j in range(700):
+        draw_square(mike, size+j)
+        mike.right(5)
+
+    window.exitonclick()
+    
+
+draw_art("turtle", "cyan", "blue", 10)
